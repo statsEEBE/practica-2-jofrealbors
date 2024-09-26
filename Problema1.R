26823c4181519dccff6d8983cb4063655c216411
jofre.albors <jofre.albors@auleseebe.upc.edu>
2024-09-26 11:28:53 +0200

    jg

--- Problema1.R
+++ Problema1.R
@@ -1,2 +1,12 @@
 #Codigo para problema 1
 
+#coeficient dtereminació és R^2
+#coeficient correlació:
+
+sqrt(0.1923)
+
+#ex3:
+0.06576#crec(és la pendent)
+#ex4:
+0.06576*647.1+11.79755
+#ex 5: mostra A pq el valor predit està per alla més o menys
